a4a89bb5946a4b4aa36754521fed2e12e5d40259
valentina lembo <valenlembo99@icloud.comgit config --global user.email valenlembo99@icloud.comgit config --global user.email valenlembo99@icloud.com>
2022-06-16 15:52:35 -0300

    primer desafio

--- js/app.js
+++ js/app.js
@@ -9,3 +9,116 @@ addEventListener('DOMContentLoaded',()=>{
         })
     }
 })
+
+// PRIMER DESAFIO
+function saludar(){
+    let nombre = prompt('¿Cómo es tu nombre?')
+    alert(`Hola ${nombre}`)
+}
+
+saludar()
+function repreguntar(){
+    let repregunta = prompt('¿Desea comprar algo mas? (si/no)')
+    if(repregunta == 'si'){
+        preguntar()
+    }else if(repregunta == 'no'){
+        alert('Gracias por su compra')
+    }else{
+        alert('Ingrese una opción valida')
+        repreguntar()
+    }
+}
+
+function deco(){
+    let opcionDeco = prompt('Nuestras opciones son: \n 1- Almohadones Varios: $5.600 \n 2- Baulera Amarilla: $15.550 \n 3- Pie de cama azul: $4.490 \n 4- Pie de cama roja: $4.490 \n 5- Portalibros: $6.550 \n 6- Alfombra: $18.000')
+    if(opcionDeco == 1 || opcionDeco == '1' || opcionDeco == 'Almohadones Varios' || opcionDeco == 'almohadones varios' || opcionDeco == 'ALMOHADONES VARIOS'){
+        alert('Su compra es: Almohadones Varios, debe abonar $5.600')
+        repreguntar()
+    }else if(opcionDeco == 2 || opcionDeco == '2' || opcionDeco == 'Baulera Amarilla' || opcionDeco == 'baulera amarilla' || opcionDeco == 'BAULERA AMARILLA'){
+        alert('Su compra es: Baulera Amarilla, debe abonar $15.550')
+        repreguntar()
+    }else if(opcionDeco == 3 || opcionDeco == '3' || opcionDeco == 'Pie de cama azul' || opcionDeco == 'pie de cama azul' || opcionDeco == 'PIE DE CAMA AZUL'){
+        alert('Su compra es: Pie de cama azul, debe abonar $4.490')
+        repreguntar()
+    }else if(opcionDeco == 4 || opcionDeco == '4' || opcionDeco == 'Pie de cama roja' || opcionDeco == 'pie de cama roja' || opcionDeco == 'PIE DE CAMA ROJA'){
+        alert('Su compra es: Pie de cama roja, debe abonar $4.490')
+        repreguntar()
+    }else if(opcionDeco == 5 || opcionDeco == '5' || opcionDeco == 'Portalibros' || opcionDeco == 'portalibros' || opcionDeco == 'PORTALIBROS'){
+        alert('Su compra es: Portalibros, debe abonar $6.550')
+        repreguntar()
+    }else if(opcionDeco == 6 || opcionDeco == '6' || opcionDeco == 'Alfombra' || opcionDeco == 'alfombra' || opcionDeco == 'ALFOMBRA'){
+        alert('Su compra es: Alfombra, debe abonar $18.000')
+        repreguntar()
+    }else{
+        alert('No se encontró la opción')
+        deco()
+    }
+}
+
+function cuadros(){
+    let opcionCuadros = prompt('Nuestras opciones son: \n 1- Cuadro de madera: $5.600 \n 2- Cuadro de metal: $15.550 \n 3- Cuadro de plastico: $4.490 \n 4- Cuadro de vidrio: $4.490 \n 5- Cuadro de cristal: $6.550 \n 6- Cuadro de piedra: $18.000')
+    if(opcionCuadros == 1 || opcionCuadros == '1' || opcionCuadros == 'Cuadro de madera' || opcionCuadros == 'cuadro de madera' || opcionCuadros == 'CUADRO DE MADERA'){
+        alert('Su compra es: Cuadro de madera, debe abonar $5.600')
+        repreguntar()
+    }else if(opcionCuadros == 2 || opcionCuadros == '2' || opcionCuadros == 'Cuadro de metal' || opcionCuadros == 'cuadro de metal' || opcionCuadros == 'CUADRO DE METAL'){
+        alert('Su compra es: Cuadro de metal, debe abonar $15.550')
+        repreguntar()
+    }else if(opcionCuadros == 3 || opcionCuadros == '3' || opcionCuadros == 'Cuadro de plastico' || opcionCuadros == 'cuadro de plastico' || opcionCuadros == 'CUADRO DE PLASTICO'){
+        alert('Su compra es: Cuadro de plastico, debe abonar $4.490')
+        repreguntar()
+    }else if(opcionCuadros == 4 || opcionCuadros == '4' || opcionCuadros == 'Cuadro de vidrio' || opcionCuadros == 'cuadro de vidrio' || opcionCuadros == 'CUADRO DE VIDRIO'){
+        alert('Su compra es: Cuadro de vidrio, debe abonar $4.490')
+        repreguntar()
+    }else if(opcionCuadros == 5 || opcionCuadros == '5' || opcionCuadros == 'Cuadro de cristal' || opcionCuadros == 'cuadro de cristal' || opcionCuadros == 'CUADRO DE CRISTAL'){
+        alert('Su compra es: Cuadro de cristal, debe abonar $6.550')
+        repreguntar()
+    }else if(opcionCuadros == 6 || opcionCuadros == '6' || opcionCuadros == 'Cuadro de piedra' || opcionCuadros == 'cuadro de piedra' || opcionCuadros == 'CUADRO DE PIEDRA'){
+        alert('Su compra es: Cuadro de piedra, debe abonar $18.000')
+        repreguntar()
+    }else{
+        alert('No se encontró la opción')
+        cuadros()
+    }
+}
+
+function velas(){
+    let opcionVelas = prompt('Nuestras opciones son: \n 1- Tall Ribbed Tower: $700 \n 2- Short Ribbed Tower: $1.000 \n 3- Tangled: $1.800 \n 4- Wabe x3: $3.300 \n 5- Set Black: $1.500 \n 6- Shell: $1.300')
+    if(opcionVelas == 1 || opcionVelas == '1' || opcionVelas == 'Tall Ribbed Tower' || opcionVelas == 'tall ribbed tower' || opcionVelas == 'TALL RIBBED TOWER'){
+        alert('Su compra es: Tall Ribbed Tower, debe abonar $700')
+        repreguntar()
+    }else if(opcionVelas == 2 || opcionVelas == '2' || opcionVelas == 'Short Ribbed Tower' || opcionVelas == 'short ribbed tower' || opcionVelas == 'SHORT RIBBED TOWER'){
+        alert('Su compra es: Short Ribbed Tower, debe abonar $1.000')
+        repreguntar()
+    }else if(opcionVelas == 3 || opcionVelas == '3' || opcionVelas == 'Tangled' || opcionVelas == 'tangled' || opcionVelas == 'TANGLED'){
+        alert('Su compra es: Tangled, debe abonar $1.800')
+        repreguntar()
+    }else if(opcionVelas == 4 || opcionVelas == '4' || opcionVelas == 'Wabe x3' || opcionVelas == 'wabe x3' || opcionVelas == 'WABE X3'){
+        alert('Su compra es: Wabe x3, debe abonar $3.300')
+        repreguntar()
+    }else if(opcionVelas == 5 || opcionVelas == '5' || opcionVelas == 'Set Black' || opcionVelas == 'set black' || opcionVelas == 'SET BLACK'){
+        alert('Su compra es: Set Black, debe abonar $1.500')
+        repreguntar()
+    }else if(opcionVelas == 6 || opcionVelas == '6' || opcionVelas == 'Shell' || opcionVelas == 'shell' || opcionVelas == 'SHELL'){
+        alert('Su compra es: Shell, debe abonar $1.300')
+        repreguntar()
+    }else{
+        alert('No se encontró la opción')
+        preguntar()
+    }
+}
+
+function preguntar(){
+    let pregunta = prompt('¿Qué te gustaría comprar? \n 1- Deco \n 2- Cuadros \n 3- Velas')
+    if(pregunta == 1 || pregunta == 'Deco' || pregunta == 'deco' || pregunta == 'DECO'){
+        deco()
+    }else if(pregunta == 2 || pregunta == 'Cuadros' || pregunta == 'cuadros' || pregunta == 'CUADROS'){
+        cuadros()
+    }else if(pregunta == 3 || pregunta == 'Velas' || pregunta == 'velas' || pregunta == 'VELAS'){
+        velas()
+    }else{
+        alert('Ingrese un valor válido')
+        preguntar()
+    }
+}
+preguntar()
+
